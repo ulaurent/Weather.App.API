377e7d004b94c79b254e65ca6b60ecbd328f70e4
Urbenson Laurent <ulaurent12@gmail.com>
2018-06-06 16:03:47 +0000

    rough with if and else toggle

--- weather_app.js
+++ weather_app.js
@@ -1,25 +1,23 @@
 // Beginning of weather app
 
-var url = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
+//var url = "https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139";
 
 //console.log(url);
 
-var request = new XMLHttpRequest();
+//var request = new XMLHttpRequest();
 
-request.open('GET', url, true);
+var lat;
+var long;
 
-request.onload = function(){
-    var data = JSON.parse(this.response);
-   // console.log(data.coord);
-   // console.log(data.weather[0].id);
-    
-    var longtitude = data.coord.lon;
-    console.log(longtitude);
-    
-    var latitude = data.coord.lat;
-    console.log(latitude);
-    
-   
-}
+if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position){
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+    })
+  }
+else{
+    console.log('Location Not authorized');
+    }
 
-request.send();
+var url = "https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+long;
+console.log(url);
